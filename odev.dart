18b8039c78b33537fba689c7c136fd2fc77668f7
Enes Yürekli <muhammedenesyurekli@gmail.com>
2024-12-08 21:36:32 +0300

    return to old algorithm

--- odev.dart
+++ odev.dart
@@ -2,27 +2,19 @@ import 'dart:math';
 
 void main() {
   var random = Random();
-  List<int> myList = [];
-  List<int> targetList = [1, 3, 5, 7, 9];
-  int attempt = 0;
-  int number;
-  while (true) {
-    int counter = 0;
-    myList.clear();
-    int i = 0;
-    while (counter < 5) {
-      number = random.nextInt(9) + 1;
-      if (number % 2 == 1 && number == targetList[i]) {
-        myList.add(number);
-        counter++;
-        i++;
-        if (counter == 5) break;
-      }
-      print(myList);
+  List<int> numbers = [0, 0, 0, 0, 0]; //the list we have
+  List<int> desiredNumbers = [1, 3, 5, 7, 9]; //the list that we want to create
+  int i = 0; //Declare the index of list
+  int attempt = 0; //Declare the number of trying
+  while (i < 5) {
+    int randomNumber = random.nextInt(11); //Generate random integer
+    numbers[i] = randomNumber; //give first integer to list
+    //checking the index of lists if it is true we increment i variable
+    if (numbers[i] == desiredNumbers[i]) {
+      i++;
       attempt++;
-    }
-    if (targetList.toString() == myList.toString()) break;
+    } else //if same index of lists is different the index is going to start point
+      i = 0;
   }
-  print(myList);
-  print("list found at $attempt. try");
+  print(attempt);
 }
